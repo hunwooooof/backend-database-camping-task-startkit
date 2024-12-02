56347f8d8f900bed1ae736b00c776ad3d21b2755
Wanyu Lee <wanyulee1998@gmail.com>
2024-12-02 21:14:37 +0800

    [feat] Complete task 1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -1,4 +1,4 @@
-
+-- =========================================
 -- █████  ████    ████  █   █     █ 
 --   █    █   █  █      █  █     ██ 
 --   █    █████   ███   ███       █ 
@@ -7,7 +7,7 @@
 -- =========================================
 -- 1. 用戶資料，資料表為 USER
 
--- 1. 新增：新增六筆用戶資料，資料如下：
+-- 1-1 新增：新增六筆用戶資料，資料如下：
 --     1. 用戶名稱為`李燕容`，Email 為`lee2000@hexschooltest.io`，Role為`USER`
 --     2. 用戶名稱為`王小明`，Email 為`wXlTq@hexschooltest.io`，Role為`USER`
 --     3. 用戶名稱為`肌肉棒子`，Email 為`muscle@hexschooltest.io`，Role為`USER`
@@ -15,15 +15,45 @@
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
 
+insert into
+    "USER" (name, email, role)
+values
+    ('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+    ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
+    ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
+    ('好野人', 'richman@hexschooltest.io', 'USER'),
+    ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
+    ('透明人', 'opacity0@hexschooltest.io', 'USER')
+
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
+
+update "USER"
+set
+    role = 'COACH'
+where
+    email in (
+        'lee2000@hexschooltest.io',
+        'muscle@hexschooltest.io',
+        'starplatinum@hexschooltest.io'
+    )
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 
+delete from "USER"
+where
+    email = 'opacity0@hexschooltest.io'
+
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
+
+select count(name) from "USER"
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
+select * from "USER"
+limit 3
 
+
+-- =========================================
 --  █████  ████    ████  █   █    ████  
 --    █    █   █  █      █  █         █ 
 --    █    █████   ███   ███       ███  
@@ -41,7 +71,7 @@
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
 
-
+-- =========================================
 -- █████  ████    ████  █   █    ████   
 --   █    █   █  █      █  █         █ 
 --   █    █████   ███   ███       ███   
@@ -65,7 +95,7 @@
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
-
+-- =========================================
 --  █████  ████    ████  █   █    █   █ 
 --    █    █   █  █      █  █     █   █ 
 --    █    █████   ███   ███      █████ 
@@ -83,7 +113,7 @@
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
 
-
+-- =========================================
 -- █████  ████    ████  █   █    █████ 
 --   █    █   █  █      █  █     █     
 --   █    █████   ███   ███      ████  
